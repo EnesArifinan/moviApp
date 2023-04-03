f6d19ecd7dfd3efbb0a325b9a3c0907cb194b9c7
Enes Arifinan <enesarifinan@gmail.com>
2023-04-03 23:53:13 +0300

    src2 eklendi

---
+++ src2/index.js
@@ -0,0 +1,12 @@
+const video = document.getElementById("vvv");
+const btn = document.getElementById("pause");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
