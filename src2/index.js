b4edf116494c351b7e87b8bcbff3fadd3551fa8e
Enes Arifinan <enesarifinan@gmail.com>
2023-04-22 19:47:15 +0300

    deleted useles file and added 'öneri' page

--- src2/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const stop = document.getElementById("pause");
-  const video = document.getElementById("videoo");
-
-  function stopVideo() {
-    video.pause();
-  }
-});
