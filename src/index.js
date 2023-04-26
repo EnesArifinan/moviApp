c4a49be393795884f3eda2a2f884149f8ad10431
Enes Arifinan <enesarifinan@gmail.com>
2023-04-26 18:29:06 +0300

    pages linked and added 'page not found' page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// video durdurup calistirma
 var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
 
@@ -10,3 +11,15 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+
+// sayfalar arsi gecis
+const elestiri = document.querySelector("#elestiri");
+const oneri = document.querySelector("#oneri");
+
+elestiri.addEventListener("click", () => {
+  window.location.href = "elestiri/index.html";
+});
+
+oneri.addEventListener("click", () => {
+  window.location.href = "oneri/index.html";
+});
---
+++ src/oneri/index.js
@@ -0,0 +1,7 @@
+addEventListener("DOMContentLoaded", () => {
+  const filmOner = document.querySelector("button");
+
+  filmOner.addEventListener("click", () => {
+    window.location.href = "../notfound.html";
+  });
+});
